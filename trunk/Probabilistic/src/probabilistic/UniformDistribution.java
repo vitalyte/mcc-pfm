d56482c88cf53d70561236da10108f61698da634
vitaly.brevus <vitaly.brevus@81b7a7e4-1ba7-11df-8cfa-b39ae38058b4>
2010-02-17 13:49:08 +0000

    Uniform

---
+++ trunk/Probabilistic/src/probabilistic/UniformDistribution.java
@@ -0,0 +1,60 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ * 
+ */
+//import ;
+package probabilistic;
+import java.lang.*;
+/**
+ *
+ * @author Alximik
+ */
+public class UniformDistribution {
+
+
+    /*'Purpose:   Calculate value of Uniform percent point function function
+*          on the given probability
+*/
+//'Inputs:
+//'   val:    probability
+//'   A:      begin of interval (location parametr)
+//'   B:      end of interval B-A: scale parametr)
+//'Returns:   value of random variable
+static double PPF(double p, double a, double b){
+    double result=0;
+    result = a + (b - a) * p;
+    return result;
+
+
+}
+
+/*Purpose:   Calculate probability of uniform cummulative
+            function on the given value  of random variable
+*/
+//'Inputs:
+//'   val:    value of random variable
+//'   A:   begin of interval (location parametr)
+//'   B:   end of interval B-A:scale parametr
+//'Returns:   probabilty
+static double CDF(double val, double a, double b){
+    double result=0;
+    result = (val - a)/(b - a);       
+    return result;
+}
+
+/*'Purpose:   Calculate value of Uniform distribution function
+//'           on the given value of random variable
+ */
+//'Inputs:
+//'   val:    probability
+//'   A:   begin of interval (location parametr)
+//'   B:   end of interval B-A:scale parametr
+//'Returns:   value of function
+static double PDF(double val, double a, double b){
+    double result=0;
+    result=1.0 / (b - a);
+    return result;
+
+}
+}
