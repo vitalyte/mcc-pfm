8ea91147fd77513801e66fa06f5b46f8b87bf35d
vitaly.brevus <vitaly.brevus@81b7a7e4-1ba7-11df-8cfa-b39ae38058b4>
2010-02-17 14:36:22 +0000

    Uniform

--- Probabilistic/src/probabilistic/UniformDistribution.java
+++ Probabilistic/src/probabilistic/UniformDistribution.java
@@ -17,9 +17,9 @@ public class UniformDistribution {
 *          on the given probability
 */
 //'Inputs:
-//'   val:    probability
-//'   A:      begin of interval (location parametr)
-//'   B:      end of interval B-A: scale parametr)
+//'   val    probability
+//'   a      begin of interval (location parametr)
+//'   b      end of interval B-A: scale parametr)
 //'Returns:   value of random variable
 static double PPF(double p, double a, double b){
     double result=0;
@@ -33,9 +33,9 @@ static double PPF(double p, double a, double b){
             function on the given value  of random variable
 */
 //'Inputs:
-//'   val:    value of random variable
-//'   A:   begin of interval (location parametr)
-//'   B:   end of interval B-A:scale parametr
+//'   val    value of random variable
+//'   a   begin of interval (location parametr)
+//'   b   end of interval B-A:scale parametr
 //'Returns:   probabilty
 static double CDF(double val, double a, double b){
     double result=0;
@@ -47,9 +47,9 @@ static double CDF(double val, double a, double b){
 //'           on the given value of random variable
  */
 //'Inputs:
-//'   val:    probability
-//'   A:   begin of interval (location parametr)
-//'   B:   end of interval B-A:scale parametr
+//'   val    probability
+//'   a   begin of interval (location parametr)
+//'   b   end of interval B-A:scale parametr
 //'Returns:   value of function
 static double PDF(double val, double a, double b){
     double result=0;
