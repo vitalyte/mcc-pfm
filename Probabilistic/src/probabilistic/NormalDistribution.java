26ec5825d256f34399f1258c31b26ee7fa9ab8c9
oljgen <oljgen@81b7a7e4-1ba7-11df-8cfa-b39ae38058b4>
2010-02-17 10:06:14 +0000

    test

---
+++ Probabilistic/src/probabilistic/NormalDistribution.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//import ;
+package probabilistic;
+
+/**
+ *
+ * @author Alximik
+ */
+public class NormalDistribution {
+static void PPF(){
+
+}
+
+static double CDF(double val, double mean, double scale){
+    double L,k,x;
+    final double   a1 = 0.31938153;
+    final double a2 = -0.356563782;
+    final double a3 = 1.781477937;
+    final double a4= -1.821255978;
+    final double a5 =  1.330274429;
+    return 0.0;
+    /*x= (val - mean) / scal;
+    L= Math.fabs(x);
+
+x = (val - mean) / scal
+L = Abs(x)
+k = 1 / (1 + 0.2316419 * L)
+cdf_Normal = 1 - 1 / Sqr(2 * pi) * Exp(-L ^ 2 / 2) * (a1 * k + a2 * k ^ 2 + a3 * k ^ 3 + a4 * k ^ 4 + a5 * k ^ 5)
+If x < 0 Then
+    cdf_Normal = 1 - cdf_Normal
+End If
+End Function*/
+}
+static void PDF(){
+    
+}
+}
