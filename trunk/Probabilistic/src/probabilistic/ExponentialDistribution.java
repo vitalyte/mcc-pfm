8e79d9413f5ad990e87c9af897611ed04e95720a
vitaly.brevus <vitaly.brevus@81b7a7e4-1ba7-11df-8cfa-b39ae38058b4>
2010-02-18 11:13:08 +0000

    ExponentialDidtribution

--- trunk/Probabilistic/src/probabilistic/ExponentialDistribution.java
+++ trunk/Probabilistic/src/probabilistic/ExponentialDistribution.java
@@ -6,57 +6,61 @@
  */
 //import ;
 package probabilistic;
-import java.lang.*;
+
 /**
  *
  * @author Alximik
  */
 public class ExponentialDistribution {
 
+    /**
+     * Calculate value of Exponential percent point function function
+     * on the given probability
+     *
+     * @param val probability
+     * @param mean average (location parametr)
+     * @param scal deviation (scale parametr)
+     *
+     * @return value of random variable
+     */
+    static double PPF(double p, double mean, double scal) {
+        double result = 0;
+        result = mean - scal * Math.log(1 - p);
+        return result;
 
-//'Purpose:   Calculate value of Exponential percent point function function
-//'           on the given probability
-//'Inputs:
-//'   val:    probability
-//'   mean:   average (location parametr)
-//'   scal:   deviation (scale parametr)
-//'Returns:   value of random variable
-static double PPF(double p, double mean, double scal){
-    double result=0;
-    result = mean - scal * Math.log(1 - p);
-    return result;
 
+    }
 
-}
+    /**
+     * Calculate probability of Exponential cummulative
+     * function on the given value  of random variable
+     *
+     * @param val value of random variable
+     * @param mean average (location parametr)
+     * @param scal deviation (scale parametr)
+     *
+     * @return probabilty
+     */
+    static double CDF(double val, double mean, double scal) {
+        double result = 0;
+        result = 1 - Math.exp(-(val - mean) / scal);
+        return result;
+    }
 
-//'*****************************************************
-//'Purpose:   Calculate probability of Exponential cummulative
-//'            function on the given value  of random variable
-//'
-//'Inputs:
-//'   val:    value of random variable
-//'   mean:   average (location parametr)
-//'   scal:   deviation (scale parametr)
-//'Returns:   probabilty
-//'*****************************************************
-static double CDF(double val, double mean, double scal){
-    double result=0;
-    result = 1 - Math.exp(-(val - mean) / scal);
-    return result;
-}
+    /**
+     * Calculate value of Exponential distribution function
+     * on the given value of random variable
+     *
+     * @param val probability
+     * @param mean average (location parametr)
+     * @param scal deviation (scale parametr)
+     *
+     * @return the value of Exponential distribution function
+     */
+    static double PDF(double val, double mean, double scal) {
+        double result = 0;
+        result = (1 / scal) * Math.exp(-1 * (val - mean) / scal);
+        return result;
 
-//'*****************************************************
-//'Purpose:   Calculate value of Exponential distribution function
-//'           on the given value of random variable
-//'Inputs:
-//'   val:    probability
-//'   mean:   average (location parametr)
-//'   scal:   deviation (scale parametr)
-//'Returns:   value of function
-static double PDF(double val, double mean, double scal){
-    double result=0;
-    result=(1 / scal) * Math.exp(-1 * (val - mean) / scal);
-    return result;
-
-}
+    }
 }
