543fb79ec7a938f9a0e027866e557be4ef488e2d
vitaly.brevus <vitaly.brevus@81b7a7e4-1ba7-11df-8cfa-b39ae38058b4>
2010-02-20 12:21:11 +0000

    ExponentialDidtribution

--- trunk/Probabilistic/src/probabilistic/UniformDistribution.java
+++ trunk/Probabilistic/src/probabilistic/UniformDistribution.java
@@ -4,58 +4,61 @@
  *
  * 
  */
-//import ;
 package probabilistic;
-import java.lang.*;
+
 /**
  *
  * @author Alximik
  */
 public class UniformDistribution {
 
+    /**
+     *Calculate value of Uniform percent point function function
+     * on the given probability
+     *
+     * @param   val probability
+     * @param   a   begin of interval (location parametr)
+     * @param   b   end of interval B-A: scale parametr)
+     *
+     * @return  value of random variable
+     */
+    static double PPF(double p, double a, double b) {
+        double result = 0;
+        result = a + (b - a) * p;
+        return result;
 
-    /*'Purpose:   Calculate value of Uniform percent point function function
-*          on the given probability
-*/
-//'Inputs:
-//'   val    probability
-//'   a      begin of interval (location parametr)
-//'   b      end of interval B-A: scale parametr)
-//'Returns:   value of random variable
-static double PPF(double p, double a, double b){
-    double result=0;
-    result = a + (b - a) * p;
-    return result;
+    }
 
+    /**
+     *Calculate probability of uniform cummulative
+     * function on the given value  of random variable
+     *
+     * @param   val value of random variable
+     * @param   a   begin of interval (location parametr)
+     * @param   b   end of interval B-A:scale parametr
+     *
+     * @return  probabilty
+     */
+    static double CDF(double val, double a, double b) {
+        double result = 0;
+        result = (val - a) / (b - a);
+        return result;
+    }
 
-}
+    /**
+     *Calculate value of Uniform distribution function
+     * on the given value of random variable
+     *
+     * @param   val probability
+     * @param   a   begin of interval (location parametr)
+     * @param   b   end of interval B-A:scale parametr
+     *
+     * @return  value of function
+     */
+    static double PDF(double val, double a, double b) {
+        double result = 0;
+        result = 1.0 / (b - a);
+        return result;
 
-/*Purpose:   Calculate probability of uniform cummulative
-            function on the given value  of random variable
-*/
-//'Inputs:
-//'   val    value of random variable
-//'   a   begin of interval (location parametr)
-//'   b   end of interval B-A:scale parametr
-//'Returns:   probabilty
-static double CDF(double val, double a, double b){
-    double result=0;
-    result = (val - a)/(b - a);       
-    return result;
-}
-
-/*'Purpose:   Calculate value of Uniform distribution function
-//'           on the given value of random variable
- */
-//'Inputs:
-//'   val    probability
-//'   a   begin of interval (location parametr)
-//'   b   end of interval B-A:scale parametr
-//'Returns:   value of function
-static double PDF(double val, double a, double b){
-    double result=0;
-    result=1.0 / (b - a);
-    return result;
-
-}
+    }
 }
