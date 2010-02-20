b273b195af3b742f697fb880c0fb77bd0b8fcd49
vitaly.brevus <vitaly.brevus@81b7a7e4-1ba7-11df-8cfa-b39ae38058b4>
2010-02-20 12:49:18 +0000

    відформатував код та коментарі

--- trunk/Probabilistic/src/probabilistic/Main.java
+++ trunk/Probabilistic/src/probabilistic/Main.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package probabilistic;
 
 import java.util.Random;
@@ -19,14 +18,15 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         //for (int i=)
-    System.out.println(NormalDistribution.CDF(0.5, 0, 1));
-    System.out.println(NormalDistribution.PDF(0.5, 0, 1));
-    System.out.println(NormalDistribution.PPF(0.999, 0, 1));
-    // testing RNG
-    Random rnd = new Random();
-    int s = -rnd.nextInt(1000000)+1;
-    Integer seed = new Integer (s);
-    for (int i=0;i<10;i++)
-        System.out.println(RNG.Ran2(seed));
+        System.out.println(NormalDistribution.CDF(0.5, 0, 1));
+        System.out.println(NormalDistribution.PDF(0.5, 0, 1));
+        System.out.println(NormalDistribution.PPF(0.999, 0, 1));
+        // testing RNG
+        Random rnd = new Random();
+        int s = -rnd.nextInt(1000000) + 1;
+        Integer seed = new Integer(s);
+        for (int i = 0; i < 10; i++) {
+            System.out.println(RNG.Ran2(seed));
+        }
     }
 }
