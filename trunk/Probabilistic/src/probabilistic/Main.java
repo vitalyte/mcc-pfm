f08a164fd3f395132183da7ee29034c7f7766592
oljgen <oljgen@81b7a7e4-1ba7-11df-8cfa-b39ae38058b4>
2010-03-03 13:18:01 +0000

    Реалізовано методи обчислення КІН для найглибшої точки і точки на поверхні

--- trunk/Probabilistic/src/probabilistic/Main.java
+++ trunk/Probabilistic/src/probabilistic/Main.java
@@ -5,6 +5,7 @@
 package probabilistic;
 
 import java.util.Random;
+import probabilistic.gui.SemiellipticalCrack;
 
 /**
  *
@@ -18,7 +19,7 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         //for (int i=)
-        System.out.println(NormalDistribution.CDF(0.5, 0, 1));
+       /* System.out.println(NormalDistribution.CDF(0.5, 0, 1));
         System.out.println(NormalDistribution.PDF(0.5, 0, 1));
         System.out.println(NormalDistribution.PPF(0.999, 0, 1));
         // testing RNG
@@ -27,6 +28,14 @@ public class Main {
         Integer seed = new Integer(s);
         for (int i = 0; i < 10; i++) {
             System.out.println(RNG.Ran2(seed));
-        }
+        }*/
+
+        //тест на поправкову функцію
+       /* SemiellipticalCrack crack = new SemiellipticalCrack();
+        crack.setLength2a(2);
+        crack.setLengthB(1);
+        crack.SIF_A(1);
+        crack.SIF_B(1);*/
+
     }
 }
