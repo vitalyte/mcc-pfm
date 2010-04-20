f72d22918ce120bd1b36d8524bf0da2e14cd85a5
vitaly.brevus <vitaly.brevus@81b7a7e4-1ba7-11df-8cfa-b39ae38058b4>
2010-04-20 15:32:31 +0000

    посортував масив

--- Probabilistic/src/probabilistic/InitiationTime.java
+++ Probabilistic/src/probabilistic/InitiationTime.java
@@ -2,11 +2,11 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package probabilistic;
 //import sun.util.
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
 
 /**
@@ -14,34 +14,46 @@ import java.util.Random;
  * @author Vitaly
  */
 public class InitiationTime {
+
     public ArrayList initTime;
     double mean;
     double scale;
     int Nmax;
-    InitiationTime (){
+
+    InitiationTime(int Nmax, double mean, double scale) {
+        initTime = new ArrayList();
+        this.Nmax = Nmax;
+        this.mean = mean;
+        this.scale = scale;
         Integer seed = -1;
         Random rnd = new Random();
         int s = -rnd.nextInt(1000000) + 1;
-        mean = 0.3;
-        scale = 34.54;
         seed = new Integer(s);
-        for (int i=0;i<Nmax;i++){
+        for (int i = 0; i < Nmax; i++) {
             double p = RNG.Ran2(seed);//ExponentialDistribution.PPF(i, seed, s)
             double time = ExponentialDistribution.PPF(p, mean, scale);
-            initTime.add(time);
+            initTime.add(new Double(time));
         }
-        //sort;
-        //initTime.
-        //SortInitTime();
-    }
-    public static ArrayList SortInitTime(ArrayList tmp)
-    {
-        return null;
-    }
-    public void PrintInitTime()
-    {
-        for (int i=0;i<Nmax;i++)
-            System.out.println((double)initTime.get(i));
+        SortInitTime(this.initTime);
     }
 
+    InitiationTime() {
+        this(100, 0.3, 34.54);
+    }
+
+    public static void SortInitTime(ArrayList tmp) {
+        Collections.sort(tmp);
+    }
+
+    public void PrintInitTime() {
+        for (int i = 0; i < Nmax; i++) {
+            System.out.println(initTime.get(i));
+        }
+    }
+
+    public static void main(String[] args) {
+        InitiationTime inittimeObj = new InitiationTime();
+        SortInitTime(inittimeObj.initTime);
+        inittimeObj.PrintInitTime();
+    }
 }
