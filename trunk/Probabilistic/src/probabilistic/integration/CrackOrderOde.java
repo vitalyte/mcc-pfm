b87d727bbdd092b78ae6507e11e8cbd145f0e7dc
vitaly.brevus <vitaly.brevus@81b7a7e4-1ba7-11df-8cfa-b39ae38058b4>
2010-07-28 21:12:17 +0000

    Виправив помилку в рінянні росту, K1/1000000

--- trunk/Probabilistic/src/probabilistic/integration/CrackOrderOde.java
+++ trunk/Probabilistic/src/probabilistic/integration/CrackOrderOde.java
@@ -17,10 +17,9 @@ import probabilistic.Simulation;
 public class CrackOrderOde implements FirstOrderDifferentialEquations {
 
     private double k1SCC;
-    
 
     public CrackOrderOde(double k1SCC, SemiellipticalCrack crack) {
-        this.k1SCC = k1SCC;      
+        this.k1SCC = k1SCC / Const.E10P6;
 
     }
 
@@ -36,16 +35,17 @@ public class CrackOrderOde implements FirstOrderDifferentialEquations {
     public void computeDerivatives(double t, double[] y, double[] yDot) throws DerivativeException {
         //throw new UnsupportedOperationException("Not supported yet.");
         //
+        double value = K1array(y)[0] - k1SCC;
         yDot[0] = 1.1 * Const.E10P_7
                 * Math.pow((2.5 * Const.E10P10
                 * Math.exp(
                 (-(3 * Const.E10P_19 - 1.5 * Const.E10P_20 * Math.pow((K1array(y)[0] - k1SCC), (1 / 3)))
-                / 7.74 * Const.E10P_21))), 0.443);
+                / (7.74 * Const.E10P_21)))), 0.443);
         yDot[1] = 1.1 * Const.E10P_7
                 * Math.pow((2.5 * Const.E10P10
                 * Math.exp(
                 (-(3 * Const.E10P_19 - 1.5 * Const.E10P_20 * Math.pow((K1array(y)[1] - k1SCC), (1 / 3)))
-                / 7.74 * Const.E10P_21))), 0.443);
+                / (7.74 * Const.E10P_21)))), 0.443);
 
     }
 
@@ -61,7 +61,7 @@ public class CrackOrderOde implements FirstOrderDifferentialEquations {
         KIA = Const.k1_A_b0 + Const.k1_A_b1 * lambda + Const.k1_A_b2 * lambda * lambda
                 + Const.k1_A_b3 * Math.pow(lambda, 3) + Const.k1_A_b4 * Math.pow(lambda, 4);
         result = Simulation.getSigma() * Math.sqrt(Math.PI * y[0] / 2) * KIA;
-        return result;
+        return result / Const.E10P6;
     }
 
     private double k1_B(double[] y) {
@@ -72,6 +72,4 @@ public class CrackOrderOde implements FirstOrderDifferentialEquations {
         result = Simulation.getSigma() * Math.sqrt(Math.PI * y[1]) * KIB;
         return result;
     }
-
-
 }
